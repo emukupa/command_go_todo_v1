3b9fa3e7182598c255a5ce60b455644cc5b2ecd3
emukupa1 <edward-manda@uhatok.com>
2022-07-11 00:02:39 -0500

    add a method to load items from file

--- todo.go
+++ todo.go
@@ -1,7 +1,11 @@
 package todo
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"time"
 )
 
@@ -48,5 +52,27 @@ func (t *Todos) Delete(index int) (bool, error) {
 
 	// delete task, everything except the index
 	*t = append(ls[:index], ls[index+1:]...)
+	return true, nil
+}
+
+func (t *Todos) Load(filename string) (bool, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// check if file exists, like the first time running the program
+		if errors.Is(err, os.ErrNotExist) {
+			return false, nil
+		}
+		return false, err
+	}
+
+	if len(data) == 0 {
+		return false, fmt.Errorf("%d data loaded", len(data))
+	}
+
+	err = json.Unmarshal(data, t)
+	if err != nil {
+		return false, err
+	}
+
 	return true, nil
 }
